8300fe905d3587e008bc39cf30b66c80cb21ea48
matiaspaulo <matias.paulo@distillery.com>
2021-07-28 11:57:52 -0300

    RecyclableMemoryStreamManager benchmarks

---
+++ RecyclableMemoryStream/RecyclableMemoryStreamDemo/Benchy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using Microsoft.IO;
+
+namespace RecyclableMemoryStreamDemo
+{
+    [MemoryDiagnoser]
+    public class Benchy
+    {
+        private RecyclableMemoryStreamManager _memoryStreamManager;
+        private List<User> _users;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _memoryStreamManager = new RecyclableMemoryStreamManager();
+            _users = PersonGenerator.Generate();
+        }
+        
+        [Benchmark]
+        public async Task SerializeRecyclableMemoryManager()
+        {
+            using (var stream = _memoryStreamManager.GetStream("memory"))
+            {
+                using (var gzip = new GZipStream(stream, CompressionLevel.Optimal, true))
+                {
+                    await JsonSerializer.SerializeAsync(gzip, _users);
+                    await gzip.FlushAsync();
+                }
+            }
+        }
+
+        [Benchmark]
+        public async Task SerializeMemoryStream()
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var gzip = new GZipStream(stream, CompressionLevel.Optimal, true))
+                {
+                    await JsonSerializer.SerializeAsync(gzip, _users);
+                    await gzip.FlushAsync();
+                }
+            }
+        }
+    }
+}
---
+++ RecyclableMemoryStream/RecyclableMemoryStreamDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace RecyclableMemoryStreamDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchy>();
+        }
+    }
+}
